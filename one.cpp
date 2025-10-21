96baf40c5958a377a63740e3d3f0219311141304
unknown <devalgupta4@gmail.com>
2025-10-21 22:47:18 +0530

    completed Lexer

---
+++ one.cpp
@@ -0,0 +1,23 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class Token(int value){
+    
+}
+int main() {
+  BinopPrecedence['<'] = 10;
+  BinopPrecedence['+'] = 20;
+  BinopPrecedence['-'] = 20;
+  BinopPrecedence['*'] = 40; 
+  fprintf(stderr, "ready> ");
+  getNextToken();
+  MainLoop();
+
+  return 0;
+}
